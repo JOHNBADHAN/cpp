aabd92c99479aaa4317fd11b96cebe32b2861dee
John Badhan <johnbadhan517@gmail.com>
2026-07-13 11:31:55 +0530

    arrays

---
+++ 07_array/array1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int chaiTemperature[5] = {85, 89, 90, 95, 84};
+
+    for(int i = 0; i < 5; i++){
+        cout << chaiTemperature[i] << " degree C \n";
+    }
+    return 0;
+}
